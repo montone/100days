d0965a50f452e628110a4b42e96a1300fdb327cd
Mike Montone <mmontone@local>
2022-08-03 20:53:56 -0400

    day001 complete

---
+++ day001/bandGenerator.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+print("Welcome to the Band Name Generator\n")
+place = input("Enter the city or place where you grew up: ")
+pet = input("Enter the name of a pet: ")
+
+print("Your Band Name is: " + place + " " + pet + "!\n")

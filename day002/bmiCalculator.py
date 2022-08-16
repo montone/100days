fc8fe254a8f1a6d41921541741dab6ebb5b61718
Mike Montone <mmontone@local>
2022-08-16 19:02:04 -0400

    day002 and day003

---
+++ day002/bmiCalculator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+sum = (float(weight)/(float(height) ** 2))
+print("BMI: " + str(round(sum, 2)))
+
+#Write your code below this line 👇
+

84070492f6a607b8d03ebc8f4c35308472632709
“Michael <mmontone@users.noreply.gitlab.com>
2024-04-26 09:51:04 -0400

    Add credentials for home use.

--- helloWorld.py
+++ helloWorld.py
@@ -13,6 +13,7 @@
 # Test: Push to github via Visual Studio 04/19/2024
 # Test: Update and push to github via CLI 04/24/2024
 # Test: git fetch.  Updating this via the web interface 04/24/2024
+# Test: git pull from home.  Updating this via the web interface 04/26/2024
 ############################################################################
 
 print("Hello World!")

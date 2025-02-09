1f648893918fc0e334d674e625869b3ec8b1ac78
Mike Montone <montone@users.noreply.github.com>
2025-02-09 16:54:50 -0500

    Day ten complete, no art.log

--- day010/calc.py
+++ day010/calc.py
@@ -8,33 +8,52 @@
 ############################################################################
 # Revisions:
 #
-#
+# Notes: Where the hell is art.log file?
 ############################################################################
 
-def add(n1, n2):
-    return n1 + n2
+def calculator():
+    def add(n1, n2):
+        return n1 + n2
 
-def subtract(n1, n2):
-    return n1 - n2
+    def subtract(n1, n2):
+        return n1 - n2
 
-def multiply(n1, n2):
-    return n1 * n2
+    def multiply(n1, n2):
+        return n1 * n2
 
-def divide(n1, n2):
-    return n1 / n2
+    def divide(n1, n2):
+        return n1 / n2
 
-# function dictionary$
-operations = {
-    "+": add,
-    "=": subtract,
-    "*": multiply,
-    "/": divide,
-}
+    # function dictionary$
+    operations = {
+        "+": add,
+        "=": subtract,
+        "*": multiply,
+        "/": divide,
+    }
 
-n1 = input("Enter a number: ")
-n2 = input("Enter a number: ")
-todo = input("Enter an operation using the following symbols +, -, * or /: ")
+    should_accumulate = True
+    n1 = float(input("Enter a number: "))
 
-print(f"You would like to {todo} using {n1} and {n2}")
-print(operations[todo](n1, n2))
+    while should_accumulate:
 
+        for symbol in operations:
+            print(symbol)
+
+        operation_symbol = input("Pick an operation: ")
+        n2 = float(input("What is the next number: "))
+
+        print(f"Using n1: {n1} and n2: {n2} Operation {operation_symbol}")
+        answer = operations[operation_symbol](n1, n2)
+        print(f"{n1} {operation_symbol} {n2} = {answer}")
+
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+
+        if choice == "y":
+            n1 = answer
+        else:
+            should_accumulate = False
+            print("\n" * 20)
+            calculator()
+
+calculator()
